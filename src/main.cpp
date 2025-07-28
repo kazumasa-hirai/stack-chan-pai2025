d5124a7437d38b469f213aaddf4b67492509e647
Kazumasa Hirai <kazumasa.hirai@kawada.co.jp>
2025-07-29 08:50:10 +0900

    add M5CoreS3_FaceDetect from https://github.com/ronron-gh/M5CoreS3_FaceDetect

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,12 @@
 #include <Stackchan_servo.h>                 // stack-chanのサーボを動かすためのライブラリ
 #include <Avatar.h>                          // 顔を表示するためのライブラリ https://github.com/meganetaaan/m5stack-avatar
 #include "formatString.hpp"                  // 文字列に変数の値を組み込むために使うライブラリ https://gist.github.com/GOB52/e158b689273569357b04736b78f050d6
+
+#include <esp_camera.h>
+#include <fb_gfx.h>
+#include <vector>
+#include "human_face_detect_msr01.hpp"
+#include "human_face_detect_mnp01.hpp"
 // ヘッダファイルのinclude end 
 // ================================== End
 
@@ -44,7 +50,48 @@ bool core_port_a = false;         // Core1のPortAを使っているかどうか
 const char* lyrics[] = { "BtnA:MoveTo90  ", "BtnB:ServoTest  ", "BtnC:RandomMode  ", "BtnALong:AdjustMode"};  // 通常モード時に表示するセリフ
 const int lyrics_size = sizeof(lyrics) / sizeof(char*);  // セリフの数
 int lyrics_idx = 0;                                      // 表示するセリフ数用の変数
+
+#define TWO_STAGE 1 /*<! 1: detect by two-stage which is more accurate but slower(with keypoints). */
+                    /*<! 0: detect by one-stage which is less accurate but faster(without keypoints). */
+
+#define LCD_WIDTH (320)
+#define LCD_HEIGHT  (240)
+#define LCD_BUF_SIZE (LCD_WIDTH*LCD_HEIGHT*2)
+
 // ================================== End
+
+static camera_config_t camera_config = {
+    .pin_pwdn     = -1,
+    .pin_reset    = -1,
+    .pin_xclk     = 2,
+    .pin_sscb_sda = 12,
+    .pin_sscb_scl = 11,
+
+    .pin_d7 = 47,
+    .pin_d6 = 48,
+    .pin_d5 = 16,
+    .pin_d4 = 15,
+    .pin_d3 = 42,
+    .pin_d2 = 41,
+    .pin_d1 = 40,
+    .pin_d0 = 39,
+
+    .pin_vsync = 46,
+    .pin_href  = 38,
+    .pin_pclk  = 45,
+
+    .xclk_freq_hz = 20000000,
+    .ledc_timer   = LEDC_TIMER_0,
+    .ledc_channel = LEDC_CHANNEL_0,
+
+    .pixel_format = PIXFORMAT_RGB565,
+    //.pixel_format = PIXFORMAT_JPEG,
+    .frame_size   = FRAMESIZE_QVGA,   // QVGA(320x240)
+    .jpeg_quality = 0,
+    .fb_count     = 2,
+    .fb_location  = CAMERA_FB_IN_PSRAM,
+    .grab_mode    = CAMERA_GRAB_WHEN_EMPTY,
+};
 
 // ------------------------------------------------
 // Stack-chan-testerの各モードを定義するエリア
@@ -158,6 +205,175 @@ void mumumuServo() {
 }
 // ============================================== End
 
+esp_err_t camera_init(){
+
+    //initialize the camera
+    M5.In_I2C.release();
+    esp_err_t err = esp_camera_init(&camera_config);
+    if (err != ESP_OK) {
+        //Serial.println("Camera Init Failed");
+        M5.Display.println("Camera Init Failed");
+        return err;
+    }
+
+    return ESP_OK;
+}
+
+void check_heap_free_size(void){
+  Serial.printf("===============================================================\n");
+  Serial.printf("Mem Test\n");
+  Serial.printf("===============================================================\n");
+  Serial.printf("esp_get_free_heap_size()                              : %6d\n", esp_get_free_heap_size() );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_DMA)               : %6d\n", heap_caps_get_free_size(MALLOC_CAP_DMA) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_SPIRAM)            : %6d\n", heap_caps_get_free_size(MALLOC_CAP_SPIRAM) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_INTERNAL)          : %6d\n", heap_caps_get_free_size(MALLOC_CAP_INTERNAL) );
+  Serial.printf("heap_caps_get_free_size(MALLOC_CAP_DEFAULT)           : %6d\n", heap_caps_get_free_size(MALLOC_CAP_DEFAULT) );
+
+}
+
+#define FACE_COLOR_WHITE  0x00FFFFFF
+#define FACE_COLOR_BLACK  0x00000000
+#define FACE_COLOR_RED    0x000000FF
+#define FACE_COLOR_GREEN  0x0000FF00
+#define FACE_COLOR_BLUE   0x00FF0000
+#define FACE_COLOR_YELLOW (FACE_COLOR_RED | FACE_COLOR_GREEN)
+#define FACE_COLOR_CYAN   (FACE_COLOR_BLUE | FACE_COLOR_GREEN)
+#define FACE_COLOR_PURPLE (FACE_COLOR_BLUE | FACE_COLOR_RED)
+
+static void draw_face_boxes(fb_data_t *fb, std::list<dl::detect::result_t> *results, int face_id)
+{
+    int x, y, w, h;
+    uint32_t color = FACE_COLOR_YELLOW;
+    if (face_id < 0)
+    {
+        color = FACE_COLOR_RED;
+    }
+    else if (face_id > 0)
+    {
+        color = FACE_COLOR_GREEN;
+    }
+    if(fb->bytes_per_pixel == 2){
+        //color = ((color >> 8) & 0xF800) | ((color >> 3) & 0x07E0) | (color & 0x001F);
+        color = ((color >> 16) & 0x001F) | ((color >> 3) & 0x07E0) | ((color << 8) & 0xF800);
+    }
+    int i = 0;
+    for (std::list<dl::detect::result_t>::iterator prediction = results->begin(); prediction != results->end(); prediction++, i++)
+    {
+        // rectangle box
+        x = (int)prediction->box[0];
+        y = (int)prediction->box[1];
+
+        // yが負の数のときにfb_gfx_drawFastHLine()でメモリ破壊してリセットする不具合の対策
+        if(y < 0){
+           y = 0;
+        }
+
+        w = (int)prediction->box[2] - x + 1;
+        h = (int)prediction->box[3] - y + 1;
+
+        //Serial.printf("x:%d y:%d w:%d h:%d\n", x, y, w, h);
+
+        if((x + w) > fb->width){
+            w = fb->width - x;
+        }
+        if((y + h) > fb->height){
+            h = fb->height - y;
+        }
+
+        //Serial.printf("x:%d y:%d w:%d h:%d\n", x, y, w, h);
+
+        //fb_gfx_fillRect(fb, x+10, y+10, w-20, h-20, FACE_COLOR_RED);  //モザイク
+        fb_gfx_drawFastHLine(fb, x, y, w, color);
+        fb_gfx_drawFastHLine(fb, x, y + h - 1, w, color);
+        fb_gfx_drawFastVLine(fb, x, y, h, color);
+        fb_gfx_drawFastVLine(fb, x + w - 1, y, h, color);
+
+#if TWO_STAGE
+        // landmarks (left eye, mouth left, nose, right eye, mouth right)
+        int x0, y0, j;
+        for (j = 0; j < 10; j+=2) {
+            x0 = (int)prediction->keypoint[j];
+            y0 = (int)prediction->keypoint[j+1];
+            fb_gfx_fillRect(fb, x0, y0, 3, 3, color);
+        }
+#endif
+    }
+}
+
+esp_err_t camera_capture_and_face_detect(){
+  Serial.println("camera_capture_and_face_detect");
+  //acquire a frame
+  M5.In_I2C.release();
+  camera_fb_t * fb = esp_camera_fb_get();
+  if (!fb) {
+    //Serial.println("Camera Capture Failed");
+    M5.Display.println("Camera Capture Failed");
+    return ESP_FAIL;
+  }
+
+
+  int face_id = 0;
+  Serial.println("HumanFaceDetect_start");
+
+#if TWO_STAGE
+  HumanFaceDetectMSR01 s1(0.1F, 0.5F, 10, 0.2F);
+  HumanFaceDetectMNP01 s2(0.5F, 0.3F, 5);
+  std::list<dl::detect::result_t> &candidates = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+  std::list<dl::detect::result_t> &results = s2.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3}, candidates);
+#else
+  HumanFaceDetectMSR01 s1(0.3F, 0.5F, 10, 0.2F);
+  std::list<dl::detect::result_t> &results = s1.infer((uint16_t *)fb->buf, {(int)fb->height, (int)fb->width, 3});
+#endif
+  Serial.println("HumanFaceDetect_end");
+  if (results.size() > 0) {
+      //Serial.printf("Face detected : %d\n", results.size());
+
+      fb_data_t rfb;
+      rfb.width = fb->width;
+      rfb.height = fb->height;
+      rfb.data = fb->buf;
+      rfb.bytes_per_pixel = 2;
+      rfb.format = FB_RGB565;
+
+      draw_face_boxes(&rfb, &results, face_id);
+      Serial.println("draw_face_boxes_end");
+  }
+
+
+  //replace this with your own function
+  //process_image(fb->width, fb->height, fb->format, fb->buf, fb->len);
+  M5.Display.startWrite();
+  M5.Display.setAddrWindow(0, 0, LCD_WIDTH, LCD_HEIGHT);
+  M5.Display.writePixels((uint16_t*)fb->buf, int(fb->len / 2));
+  M5.Display.endWrite();
+  
+  Serial.println("M5_Display_end");
+  //Serial.println("<heap size before fb return>");  
+  //check_heap_free_size();
+
+  //return the frame buffer back to the driver for reuse
+  esp_camera_fb_return(fb);
+  Serial.println("esp_camera_fb_return");
+
+  //Serial.println("<heap size after fb return>");  
+  //check_heap_free_size();
+
+  return ESP_OK;
+}
+
+void camera_capture_and_face_detect_loop(){
+  Serial.println("loop_start");
+  for (;;) {
+    M5.update();
+    if (M5.BtnB.wasPressed()) {
+      // ランダムモードを抜ける処理。(loop()に戻ります。)
+      Serial.println("break");
+      break;
+    }
+    camera_capture_and_face_detect();
+  }
+  Serial.println("loop_end");
+}
 
 // ------------------------------------------------------------------
 // Arduinoフレームワークで一番最初に実行される関数の定義
@@ -166,6 +382,7 @@ void mumumuServo() {
 void setup() {
   auto cfg = M5.config();       // 設定用の情報を抽出
   cfg.serial_baudrate = 115200;
+  //cfg.output_power = true;
   M5.begin(cfg);                // M5Stackをcfgの設定で初期化
   M5.setTouchButtonHeight(40); // タッチボタンの画面下部から40pxに設定
   M5.Log.setLogLevel(m5::log_target_display, ESP_LOG_NONE);    // M5Unifiedのログ初期化（画面には表示しない。)
@@ -209,6 +426,8 @@ void setup() {
   last_mouth_millis = millis();    // loop内で使用するのですが、処理を止めずにタイマーを実行するための変数です。一定時間で口を開くのとセリフを切り替えるのに利用します。
   //moveRandom();
   //testServo();
+
+  camera_init();
 }
 
 
@@ -224,7 +443,8 @@ void loop() {
   }
   
   if (M5.BtnB.wasSingleClicked()) {               // ボタンBが1回クリックされたとき
-    testServo();                                  // サーボのテストを実行します。
+    //testServo();                                  // サーボのテストを実行します。
+    camera_capture_and_face_detect_loop();
   } else if (M5.BtnB.wasDoubleClicked()) {        // ボタンBをダブルクリックしたとき
     // Groveポートの出力を切り替えます。
     if (M5.Power.getExtOutput() == true) { 
